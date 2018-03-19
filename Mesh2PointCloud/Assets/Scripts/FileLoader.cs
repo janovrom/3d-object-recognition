532aef342165002dbbc571197324fc1793bb34aa
Roman Janovsky <roman.janovsky9@seznam.cz>
2018-03-19 13:57:10 +0100

    added convertor from mesh to point cloud in unity using rasterization and camera fragment positions

---
+++ Mesh2PointCloud/Assets/Scripts/FileLoader.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class FileLoader : MonoBehaviour {
+
+    public bool SwitchZY = true;
+    private string _outputDirectory;
+
+	// Use this for initialization
+	void Start () {
+        _outputDirectory = Path.Combine(Directory.GetCurrentDirectory(), "data-out");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SavePoints(Color[] positions, int Width, int Height, string name)
+    {
+        // Convert texture to point cloud
+        using (StreamWriter writer = new StreamWriter(File.Create(Path.Combine(_outputDirectory, name))))
+        {
+            for (int i = 0; i < Width; ++i)
+            {
+                for (int j = 0; j < Height; ++j)
+                {
+                    Color c = positions[j * Width + i];
+                    float a = c.a;
+                    float r = c.r;
+                    float g = c.g;
+                    float b = c.b;
+
+                    if (Mathf.Abs(a) < 0.001 && Mathf.Abs(r) < 0.001 && Mathf.Abs(g) < 0.001 && Mathf.Abs(b) < 0.001)
+                    {
+                        continue;
+                    }
+
+                    writer.Write(r);
+                    writer.Write(" ");
+                    writer.Write(g);
+                    writer.Write(" ");
+                    writer.Write(b);
+                    writer.Write("\n");
+                }
+            }
+        }
+    }
+
+    public GameObject LoadMesh(string path)
+    {
+        Debug.Log("Loading mesh from " + path);
+        var lines = File.ReadAllLines(path);
+        // Check if it is really off file format
+        Debug.Assert(lines[0].Equals("OFF"));
+        // Read the header
+        var header = lines[1].Split(' ');
+        int numVertices = 0;
+        int numFaces = 0;
+        int numEdges = 0;
+        bool success = true;
+        success &= int.TryParse(header[0], out numVertices);
+        success &= int.TryParse(header[1], out numFaces);
+        success &= int.TryParse(header[2], out numEdges);
+        // Check header validity
+        Debug.Assert(success, "Off file header is not valid!");
+        int header_size = 2;
+        Vector3[] vertices = new Vector3[numVertices];
+        int[] indices = new int[numFaces * 3];
+
+        // Read vertices
+        for (int i = header_size; i < numVertices + header_size; ++i)
+        {
+            var splitted = lines[i].Split(' ');
+            float x, y, z;
+            if (SwitchZY)
+            {
+                success &= float.TryParse(splitted[0], out x);
+                success &= float.TryParse(splitted[1], out z);
+                success &= float.TryParse(splitted[2], out y);
+            }
+            else
+            {
+                success &= float.TryParse(splitted[0], out x);
+                success &= float.TryParse(splitted[1], out y);
+                success &= float.TryParse(splitted[2], out z);
+            }
+            Debug.Assert(success, "Couldn't parse " + (i - 2).ToString() + " vertex!");
+
+            vertices[i - 2] = new Vector3(x, y, z);
+        }
+
+        // Read indices
+        for (int i = numVertices + header_size; i < numVertices + header_size + numFaces; ++i)
+        {
+            int ii = i - numVertices - header_size;
+            var splitted = lines[i].Split(' ');
+            Debug.Assert(splitted.Length == 4, "Reading wrong line for face " + (ii).ToString() + " or mesh is not triangulated!");
+            int cc;
+            int idx1, idx2, idx3;
+            // Read index count for this face
+            success &= int.TryParse(splitted[0], out cc);
+            Debug.Assert(success, "Something went wrong when parsing " + lines[i]);
+            Debug.Assert(cc == 3, "Face " + (ii).ToString() + " is not a triangle.");
+
+            // Read indices for this face
+            success &= int.TryParse(splitted[1], out idx1);
+            success &= int.TryParse(splitted[2], out idx2);
+            success &= int.TryParse(splitted[3], out idx3);
+            Debug.Assert(success, "Something went wrong when parsing " + lines[i]);
+
+            indices[ii * 3 + 0] = idx1;
+            indices[ii * 3 + 1] = idx2;
+            indices[ii * 3 + 2] = idx3;
+
+            // Compute normal
+            Vector3 v2v1 = vertices[idx2] - vertices[idx1];
+            Vector3 v3v1 = vertices[idx3] - vertices[idx1];
+            Vector3 n = Vector3.Cross(v2v1, v3v1);
+        }
+
+        GameObject go = new GameObject(Path.GetFileNameWithoutExtension(path));
+        go.AddComponent<MeshRenderer>();
+        MeshFilter mf = go.AddComponent<MeshFilter>();
+
+        if (mf.sharedMesh != null)
+        {
+            mf.sharedMesh.Clear();
+        }
+        else
+        {
+            mf.sharedMesh = new Mesh();
+        }
+
+        mf.sharedMesh.vertices = vertices;
+        mf.sharedMesh.triangles = indices;
+        mf.sharedMesh.RecalculateBounds();
+        mf.sharedMesh.RecalculateNormals();
+        return go;
+    }
+}
---
+++ Mesh2PointCloud/Assets/Scripts/FileLoaderEditor.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+
+[CustomEditor(typeof(FileLoader))]
+public class FileLoaderEditor : Editor {
+
+
+    private static GUIContent[] _dropdownContent;
+    private int _selectedItem = 0;
+
+
+    static FileLoaderEditor()
+    {
+        _dropdownContent = new GUIContent[0];
+    }
+
+    private void HandleDropdown()
+    {
+        _selectedItem = EditorGUILayout.Popup(new GUIContent("Selected Object:"), _selectedItem, _dropdownContent);
+    }
+
+    private void HandleReload()
+    {
+        GUIContent content = new GUIContent();
+        content.text = "Reload";
+        content.tooltip = "Reloads files in directory.";
+        if (GUILayout.Button(content))
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "data");
+            if (Directory.Exists(path))
+            {
+                var files = Directory.GetFiles(path);
+                _dropdownContent = new GUIContent[files.Length];
+                for (int i = 0; i < files.Length; ++i)
+                {
+                    _dropdownContent[i] = new GUIContent();
+                    _dropdownContent[i].text = Path.GetFileName(files[i]);
+                    _dropdownContent[i].tooltip = files[i];
+                }
+                
+            }
+            else
+            {
+                Debug.LogWarning(string.Format("Directory {0} not found!", path));
+            }
+        }
+    }
+
+    private void HandleAxisTick()
+    {
+        GUIContent content = new GUIContent();
+        content.text = "Switch Z/Y axes";
+        content.tooltip = "Unity might use different axes than your file format. And it's probably switched Z,Y.";
+        ((FileLoader)target).SwitchZY = GUILayout.Toggle(((FileLoader)target).SwitchZY, content);
+    }
+
+    private void HandleLoading()
+    {
+        GUIContent content = new GUIContent();
+        content.text = "Load .off";
+        content.tooltip = "Loads selected .off file.";
+        //EditorGUILayout.DropdownButton(content, FocusType.Keyboard);
+        if (GUILayout.Button(content))
+        {
+            if (_dropdownContent.Length > 0)
+            {
+                var filepath = _dropdownContent[_selectedItem].tooltip;
+                ((FileLoader)target).LoadMesh(filepath);
+            }
+        }
+    }
+
+    public override void OnInspectorGUI()
+    {
+        HandleDropdown();
+        HandleReload();
+        HandleAxisTick();
+        HandleLoading();
+    }
+}
+#endif
