8d08c622fe2cb48aeebd6e6113bde620a4f789f5
Roman Janovsky <roman.janovsky9@seznam.cz>
2018-05-07 09:44:33 +0200

    added missing files

---
+++ Mesh2PointCloud/Assets/Scripts/Scenario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+[Serializable]
+public class Scenario
+{
+
+    public float    StartRotationY  = 0.0f;
+    public float    StartRotationX  = 25.0f;
+    public float    RotationStepY   = 30.0f;
+    public float    RotationStepX   = 25.0f;
+    public int      StopStepY       = 12;
+    public int      StopStepX       = 3;
+
+}
