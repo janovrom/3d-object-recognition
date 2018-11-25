fix to rotation of points in augment batch

shapenetparts.py
@@ -204,7 +204,7 @@ class Parts(dataset_template):
                 per_point_noise_range = 0.0
                 p = np.copy(data[4])
                 orig_shape = data[4].shape
-                p = np.reshape(p, [3,-1])
+                p = np.reshape(p, [-1,3]).transpose()
                 p = convert.rotatePoints(p, convert.eulerToMatrix((0,np.random.randint(0,360),0))) # random rotation
                 p = p.transpose()
                 p = p * (1.0 + scale_range * (np.array([np.random.randint(0,500),np.random.randint(0,500),np.random.randint(0,500)]) / 500.0 - 0.5) / 5.0) # random scale in range 1 +- scale_range*0.1