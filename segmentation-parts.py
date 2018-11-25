fix to rotation of points

segmentation-parts.py
@@ -438,11 +438,13 @@ class PartsNet():
                 acc = acc + a
                 acc_cat = acc_cat + np.sum(cat == predicted_category) / predicted_category.shape[0]
                 
-                for j in range(0, deconvolved_images.shape[0]):
-                    if interpolate:
-                        dataset.save_segmentation(lbs[j], seg_vec[j], names[j], points[j], data_dict, in_memory=in_memory, interpolate=interpolate)
-                    else:
-                        dataset.save_segmentation(lbs[j], deconvolved_images[j], names[j], points[j], data_dict, in_memory=in_memory, interpolate=interpolate)
+                dataset.save_segmentation_vote_mem(lbs, deconvolved_images, names[0], points, data_dict)
+
+                # for j in range(0, deconvolved_images.shape[0]):
+                #     if interpolate:
+                #         dataset.save_segmentation(lbs[j], seg_vec[j], names[j], points[j], data_dict, in_memory=in_memory, interpolate=interpolate)
+                #     else:
+                #         dataset.save_segmentation(lbs[j], deconvolved_images[j], names[j], points[j], data_dict, in_memory=in_memory, interpolate=interpolate)
  
                 if visualize:
                     for j in range(0, deconvolved_images.shape[0]):
@@ -490,7 +492,8 @@ class PartsNet():
                     stime = time.time()
                     for i in range(batches):
                         # occ,seg,cat,names,_,_,wgs = self.dataset.next_mini_batch_augmented(self.dataset.train)
-                        occ,seg,cat,names,_,_,wgs = self.dataset.next_mini_batch(self.dataset.train,augment=augment)
+                        # occ,seg,cat,names,_,_,wgs = self.dataset.next_mini_batch(self.dataset.train,augment=augment)
+                        occ,seg,cat,names,_,_,wgs = self.dataset.next_vote_mini_batch(self.dataset.train)
                         wgs = (wgs - min_wgs) / (max_wgs - min_wgs) # normalize in range (0,1)
                         wgs = 1.0 - wgs # best results should have least priority
                         summary,_,d_cost = sess.run([summary_op,train_op,cost], feed_dict={X: occ, Y_cat: cat, Y_seg: seg, keep_prob: self.keep_prob, bn_training: True, weight: wgs})
@@ -627,4 +630,4 @@ class PartsNet():
 if __name__ == "__main__":
     # s = PartsNet("ShapeNet", "./3d-object-recognition/UnityData")
     s = PartsNet("ShapeNet", "./3d-object-recognition/ShapePartsData")
-    s.run_model(load=False, train=True,visualize=False, in_memory=True,interpolate=False,augment=True)
+    s.run_model(load=True, train=False,visualize=False, in_memory=True,interpolate=False,augment=True)

shapenetparts.py
@@ -195,14 +195,14 @@ class Parts(dataset_template):
         lbs = []
         acc = []
 
-        data_idx = dataset[dataset_template.CURRENT_BATCH]
+        data_idx = dataset[Parts.ORDER][dataset[dataset_template.CURRENT_BATCH]]
         data = dataset[dataset_template.DATASET][data_idx]
 
-        for _ in range(0, 360, 360 / self.batch_size):
+        for rot in range(-15, 30, 15):
             p = np.copy(data[4])
             orig_shape = data[4].shape
-            p = np.reshape(p, [3,-1])
-            p = convert.rotatePoints(p, convert.eulerToMatrix((0,np.random.randint(0,360),0))) # random rotation
+            p = np.reshape(p, [-1,3]).transpose()
+            p = convert.rotatePoints(p, convert.eulerToMatrix((0,rot,0))) # random rotation
             p = p.transpose()
             p = np.reshape(p, orig_shape)
             occupancy_grid,label_grid,_,_,_ = dl.load_binvox_np(p, data[5])
@@ -457,12 +457,13 @@ class Parts(dataset_template):
         name_split = name.split("-")
         cat_dir = name_split[0]
         parts = Parts.Labels[cat_dir]
-        parts[Parts.IOU_GROUND_TRUTH].append(segmentation_gt_pts)
+        parts[Parts.IOU_GROUND_TRUTH].append(segmentation_gt_pts[0])
         # convert point cloud and grid results to point cloud results
         num_parts = 50
-        counter = np.zeros((points[0].shape[0], num_parts))
+        counter = np.zeros((int(points.shape[1]/3), num_parts))
         for i in range(0, points.shape[0]):
             pointcloud = np.array(points[i])
+            seg = segmentation_res[i]
             num_points = int(pointcloud.shape[0])
             # Find point cloud min and max
             min_x = np.min(pointcloud[0::3])
@@ -483,15 +484,15 @@ class Parts(dataset_template):
             cz = size_z / 2 + min_z
             extent = int(self.shape[0] / 2)
 
-            for i in range(0,num_points,3):
-                x = pointcloud[i+0]
-                y = pointcloud[i+1]
-                z = pointcloud[i+2]
+            for j in range(0,num_points,3):
+                x = pointcloud[j+0]
+                y = pointcloud[j+1]
+                z = pointcloud[j+2]
                 idx_x = int(((x - cx) * extent / max_size + extent) * (self.oshape[0] - 1) / (extent * 2))
                 idx_y = int(((y - cy) * extent / max_size + extent) * (self.oshape[0] - 1) / (extent * 2))
                 idx_z = int(((z - cz) * extent / max_size + extent) * (self.oshape[0] - 1) / (extent * 2))
-                idx = int(i/3)
-                counter[idx,segmentation_res[idx_x,idx_y,idx_z]] += 1
+                idx = int(j/3)
+                counter[idx,seg[idx_x,idx_y,idx_z]] += 1
 
 
         segmentation_res_pts = np.argmax(counter, axis=-1)