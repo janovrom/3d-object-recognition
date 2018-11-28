791547ed8ed55b285837947f8d4d7ec61f3bb7b7
Roman Janovsky <janovrom@fel.cvut.cz>
2018-11-28 11:32:28 +0100

    changes

--- shapenetparts.py
+++ shapenetparts.py
@@ -200,14 +200,14 @@ class Parts(dataset_template):
         for data_idx in dataset[Parts.ORDER][start:end]:
             data = dataset[dataset_template.DATASET][data_idx]
             if augment:
-                scale_range = 0.0
+                scale_range = 1.0
                 per_point_noise_range = 0.0
                 p = np.copy(data[4])
                 orig_shape = data[4].shape
                 p = np.reshape(p, [-1,3]).transpose()
-                p = convert.rotatePoints(p, convert.eulerToMatrix((0,np.random.randint(0,360),0))) # random rotation
+                # p = convert.rotatePoints(p, convert.eulerToMatrix((0,np.random.randint(0,360),0))) # random rotation
                 p = p.transpose()
-                # p = p * (1.0 + scale_range * (np.array([np.random.randint(0,500),np.random.randint(0,500),np.random.randint(0,500)]) / 500.0 - 0.5) / 5.0) # random scale in range 1 +- scale_range*0.1
+                p = p * (1.0 + scale_range * (np.array([np.random.randint(0,500),np.random.randint(0,500),np.random.randint(0,500)]) / 500.0 - 0.5) / 5.0) # random scale in range 1 +- scale_range*0.1
                 # p = p * ((np.random.rand() * 0.2 - 0.1) * per_point_noise_range + 1.0)
                 # p = p.transpose()
                 p = np.reshape(p, orig_shape)
